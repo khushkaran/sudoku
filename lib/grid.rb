a8744f55acd41213fc063ceb58a7390eff7d5934
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-14 07:35:35 +0000

    Extracted out neighbours function

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,7 +1,9 @@
 require_relative './cell'
+require_relative './neighbours'
 
 class Grid
-  ROWS = (0..80).each_slice(9).to_a
+  include Neighbours
+  
   BOX_ROWS = (0..80).each_slice(3).to_a
   attr_reader :cells
   attr_accessor :puzzle
@@ -18,23 +20,7 @@ class Grid
   def boxes
     [0,0,0,6,6,6,12,12,12].map.with_index{|e, i| BOX_ROWS[i+e] + BOX_ROWS[i+e+3] + BOX_ROWS[i+e+6]}
   end
-
-  def values_in_neighbours_of(index)
-    unique_neighbours_of(index).map{|neighbour| puzzle[neighbour].to_i}.reject{|neighbour| neighbour == 0}.uniq.sort
-  end
-
-  def unique_neighbours_of(index)
-    all_neighbours_of(index).reject{|neighbour| neighbour == index}.uniq
-  end
-
-  def all_neighbours_of(index)
-    current(boxes,index) + current(ROWS,index) + current(ROWS.transpose,index)
-  end
-
-  def current(array,index)
-    array.detect {|element| element.include?(index)}
-  end
-
+  
   def try_to_solve
     cells.flatten.each {|c| c.solve}
     update_neighbours
